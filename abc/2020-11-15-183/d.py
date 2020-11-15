dc61bd68ecd870e03d49fe7c59906f9546bfc3b4
Risa Nishiyama <lisa.nishiyama@gmail.com>
2020-11-16 01:00:26 +0900

    Modify 183 d
    いもす法を使う

--- abc/2020-11-15-183/d.py
+++ abc/2020-11-15-183/d.py
@@ -1,19 +1,30 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class Event:
+    time: int
+    use: int  # Negative if someone stop using the water
+
+
 def main():
     N, W = [int(x) for x in input().split()]
-    water_plan = [0 for x in range(0, 2 * 100000)]
-    can_serve = True
+    events = []
     for n in range(0, N):
         S, T, P = [int(x) for x in input().split()]
-        if not can_serve:
-            # No need to update the plan
-            continue
-        for i in range(S, T):
-            water_plan[i] += P
-            if water_plan[i] > W:
-                can_serve = False
-                break
-    print("Yes" if can_serve else "No")
- 
- 
+        events.append(Event(S, P))
+        events.append(Event(T, -P))
+    events.sort(key=lambda e: (e.time, e.use))
+    # print(events)
+    use = 0
+    for e in events:
+        use += e.use
+        if use > W:
+            # Since events are sorted by time and use, negative use comes earlier than positive at the same time
+            print("No")
+            return
+    print("Yes")
+
+
 if __name__ == "__main__":
     main()
