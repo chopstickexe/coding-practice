da2e0c7d50e519411fb7cc107d4bd0172cc6fdb0
Risa Nishiyama <lisa.nishiyama@gmail.com>
2021-01-31 21:40:53 +0900

    Add abc190 c
    Align to the official python sample code

--- abc/2021-01-30-190/c.py
+++ abc/2021-01-30-190/c.py
@@ -1,31 +1,26 @@
+import itertools
+
+
 def main():
     N, M = [int(x) for x in input().split()]
-    conds = []
-    for i in range(M):
-        A, B = [int(x) for x in input().split()]
-        conds.append((A, B))
-    # print(conds)
+    conds = [(int(A), int(B)) for A, B in [input().split() for _ in range(M)]]
     # print("***")
+    # print(conds)
+
     K = int(input())
-    choices = []
-    for i in range(K):
-        C, D = [int(x) for x in input().split()]
-        choices.append((C, D))
+    choices = [(int(C), int(D)) for C, D in [input().split() for _ in range(K)]]
+    # print("***")
+    # print(choices)
 
     ans = 0
-    for x in range(2 ** K):
-        # 2 ** 16 == 65536
-        selection = set()
-        for i, c in enumerate(f"{x:b}".zfill(K)):
-            dish = choices[i][int(c)]
-            selection.add(dish)
-        matched = 0
-        for i in range(M):
-            if all([d in selection for d in conds[i]]):
-                matched += 1
-                # print(f"matched: condition = {conds[i]}, selection = {selection}")
-        if matched > ans:
-            ans = matched
+    for balls in itertools.product(*choices):  # for all combinations of items in tuples
+        # print("***")
+        # print(balls)
+        balls = set(balls)
+        count = sum(A in balls and B in balls for A, B in conds)
+        if ans < count:
+            ans = count
+
     print(ans)
 
 
