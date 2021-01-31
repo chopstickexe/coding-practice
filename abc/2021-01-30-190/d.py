e40c812b7990948d559bd3a3285aeeeed7897cc9
Risa Nishiyama <lisa.nishiyama@gmail.com>
2021-01-31 22:16:46 +0900

    Add abc190 d

--- abc/2021-01-30-190/d.py
+++ abc/2021-01-30-190/d.py
@@ -1,50 +1,30 @@
-def factorization(n):
-    arr = []
-    temp = n
-    for i in range(2, int(-(-(n ** 0.5) // 1)) + 1):
-        if temp % i == 0:
-            cnt = 0
-            while temp % i == 0:
-                cnt += 1
-                temp //= i
-            arr.append([i, cnt])
-
-    if temp != 1:
-        arr.append([temp, 1])
-
-    if arr == []:
-        arr.append([n, 1])
-
-    return arr
+def make_divisors(n):
+    lowers, uppers = [], []
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            # print(f"lower->{i}")
+            lowers.append(i)
+            x = n // i
+            if x != i:
+                # print(f"upper->{x}")
+                uppers.append(x)
+        i += 1
+    return lowers + uppers[::-1]
 
 
 def main():
     N = int(input())
     N *= 2
     # Find integers satisfy (2S + M - 1) * M = 2N
-    factors_tuples = factorization(N)
-    factors = []
-    for x in factors_tuples:
-        for _ in range(x[1]):
-            factors.append(x[0])
-    # print(factors)
-    num_of_factors = len(factors)
+    divisors = make_divisors(N)
+    # print(divisors)
 
     count = 0
-    cache = set()
-    for x in range(2 ** num_of_factors):
-        M = 1
-        for i, c in enumerate(f"{x:b}".zfill(num_of_factors)):
-            if c == "1":
-                M *= factors[i]
-        if M in cache:
-            continue
-        cache.add(M)
-        # print(f"M={M}")
+    for M in divisors:
         X = N // M
-        # print(f"N={N}")
-        if (X + 1 - M) % 2 == 0:
-            # print(f"M={M}")
+        if (X - (M - 1)) % 2 == 0:
+            # print(f"M={M}, S={(X - (M - 1)) / 2}")
             count += 1
     print(count)
 
