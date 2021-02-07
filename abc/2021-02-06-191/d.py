c5f14c463ad3359ed33823d6679615649b10d397
Risa Nishiyama <lisa.nishiyama@gmail.com>
2021-02-07 15:06:56 +0900

    Add abc191 d

--- abc/2021-02-06-191/d.py
+++ abc/2021-02-06-191/d.py
@@ -1,20 +1,18 @@
+from decimal import Decimal
 import math
 
 
 def main():
-    X, Y, R = [int(float(x) * 10000) for x in input().split()]
+    # Ref: https://atcoder.jp/contests/abc191/submissions/20025185
+    X, Y, R = [Decimal(x) for x in input().split()]
     # Find integers x & y that satisfy the conditions
     count = 0
-    x_min = math.ceil((X - R) / 10000)
-    x_max = math.floor((X + R) / 10000) + 1
+    x_min = math.ceil(X - R)
+    x_max = math.floor(X + R) + 1
     for x in range(x_min, x_max):
-        x *= 10000
-        temp = R ** 2 - (x - X) ** 2
-        if temp < 0:
-            continue
-        temp = math.sqrt(temp)
-        min_y = math.ceil((Y - temp) / 10000)
-        max_y = math.floor((Y + temp) / 10000)
+        temp = (R ** 2 - (x - X) ** 2).sqrt()
+        min_y = math.ceil(Y - temp)
+        max_y = math.floor(Y + temp)
         # print(f"x={x // 10000}, max_y={max_y}, min_y={min_y}")
         count += (max_y - min_y + 1)
     print(count)
