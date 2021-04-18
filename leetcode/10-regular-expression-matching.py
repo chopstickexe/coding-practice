042711df9c160dc4defe6a738d3f7f9a874190be
Lisa Nishiyama <lisa.nishiyama@gmail.com>
2021-04-18 20:22:11 +0900

    [leetcode] add 10 (recursion approach)

---
+++ leetcode/10-regular-expression-matching.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/regular-expression-matching/
+
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        # print(f"s={s}, p={p}")
+        if not p:
+            return not s
+
+        first_match = bool(s) and p[0] in {s[0], "."}
+
+        if len(p) >= 2 and p[1] == "*":
+            return (self.isMatch(s, p[2:])) or (first_match and self.isMatch(s[1:], p))
+        else:
+            return first_match and self.isMatch(s[1:], p[1:])
